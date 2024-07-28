ee7d0c06f36453881a3af99abbcba6c62261bfe7
Morales Bryan <152868991+bmoralesKrugerschool@users.noreply.github.com>
2024-07-28 18:28:07 -0500

    token mejora

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -6,21 +6,24 @@ import jwt from 'jsonwebtoken';
 import { TOKEN_SECRET } from '../config.js';
 import ApiResponse from '../../utils/apiResponse.js';
 
+// Middleware para validar el token
 export const validateToken = async (req, res, next) => {
     console.info('Ingreso a validateToken middleware');
 
-    const { token } = req.cookies;
+    const token = req.cookies?.token; // Asegúrate de que la cookie esté definida
     
     if (!token) {
+        console.warn('Token no encontrado en las cookies');
         return res.status(401).json(ApiResponse.error(401, 'No autorizado', null));
     }
+
     try {
         const decoded = jwt.verify(token, TOKEN_SECRET);
         req.user = decoded;
         console.info('Token verificado:', decoded);
         next();
     } catch (error) {
-        console.error('Error:', error);
+        console.error('Error al verificar el token:', error);
         return res.status(500).json(ApiResponse.error(500, 'No autorizado', null));
     } 
-}   
+};
--- src/users/router.js
+++ src/users/router.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {getAllUsers, updateUser,getUser, getUserId} from './controller.js';
+import {getAllUsers, updateUser} from './controller.js';
 import { validateToken } from "../middlewares/validateToken.js";
 import { isLandlord, isAdmin } from '../auth/controller.js';
 
@@ -7,6 +7,4 @@ const router = Router();
 
 router.get('/getAllUsers',validateToken, getAllUsers); //ruta para obtener todos los usuarios
 router.post('/updateUser',validateToken,isLandlord, updateUser); //ruta para actualizar un usuario
-router.get('/getUser',validateToken,isAdmin, getUser); //ruta para obtener un usuario
-router.get('/getUserId',validateToken,isAdmin, getUserId); //ruta para obtener un usuario
 export default router;
